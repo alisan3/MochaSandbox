6924f74311ffaf1f7f0773aa17ddd09c36d4238e
Alexander Bohle <alexander.bohle@gmail.com>
2026-06-04 16:54:15 +0100

    BindHandlersExplicitly

--- OrderService/Program.cs
+++ OrderService/Program.cs
@@ -12,11 +12,18 @@ builder
     .AddOrderService()
     .AddRabbitMQ(t =>
     {
-        t.DeclareExchange(WellKnown.Exchanges.Events).Type(RabbitMQExchangeType.Topic).Durable();
+        t.BindHandlersExplicitly(); // no per-handler auto queues/consumers
+        t.AutoProvision(false); // don't declare anything not opted-in
+
+        t.DeclareExchange(WellKnown.Exchanges.Events)
+            .Type(RabbitMQExchangeType.Topic)
+            .Durable()
+            .AutoProvision(true);
 
         t.DeclareQueue(WellKnown.Queues.OrderServiceEvents)
             .Durable()
-            .WithArgument("x-queue-type", RabbitMQQueueType.Quorum);
+            .WithArgument("x-queue-type", RabbitMQQueueType.Quorum)
+            .AutoProvision(true);
 
         t.DeclareBinding(WellKnown.Exchanges.Events, WellKnown.Queues.OrderServiceEvents)
             //! Multiple routing keys can't be added to the same queue, multiple routes work only with wildcards
